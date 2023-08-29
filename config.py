a5a501a33673fdb751a3f1423f8be1a84768285f
JDLAU1213 <143489450+JDLAU1213@users.noreply.github.com>
2023-08-29 15:52:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.cevxrx4bkdo9.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "laujundian-employee"
+custombucket = "laujundian-s3-employee"
 customregion = "us-east-1"
 
